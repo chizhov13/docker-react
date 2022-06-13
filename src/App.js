4d7f4a36d24a367e4acc378f3bd889778f43145c
Dmitriy Chizhov <dc@zaproo.com>
2022-06-13 19:55:08 +0200

    Changes in App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I did a change
         </a>
       </header>
     </div>
